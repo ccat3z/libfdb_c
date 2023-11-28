10c677f8132b4a12399f0b8e6d1bc621c99c3dc9
ccat3z <fzhang.chn@outlook.com>
2023-11-28 15:18:40 +0800

    cmake: Add missing depends and remove library type for fdb_c

--- extractor/workdir/cmake/fdb_c.cmake
+++ extractor/workdir/cmake/fdb_c.cmake
@@ -18,9 +18,9 @@ endif()
 
 set(FDB_C_ASM_DIR ${CMAKE_CURRENT_SOURCE_DIR}/${os}-${cpu})
 
-add_library(fdb_c SHARED fdb_c.cpp fdb_c.actor.g.cpp ${FDB_C_ASM_DIR}/fdb_c.g.S)
+add_library(fdb_c fdb_c.cpp fdb_c.actor.g.cpp ${FDB_C_ASM_DIR}/fdb_c.g.S)
 target_include_directories(fdb_c
     PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}
     PRIVATE ${FDB_C_ASM_DIR}
 )
-target_link_libraries(fdb_c PRIVATE fdbclient)
+target_link_libraries(fdb_c PRIVATE fdbclient fdbdeps)
